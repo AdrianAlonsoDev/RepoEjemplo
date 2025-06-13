b51d62fcd846fc8bd90569c6ae719345f05e4b89
Jorge <jorgecabaserrano@gmail.com>
2025-06-13 19:20:30 +0200

    add clientRoute

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/homePage'
+import { ClientPage } from './pages/ClientPage'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/clients" element={<ClientPage />} />
+        {/* Agrega más rutas aquí */}
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
